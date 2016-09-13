2b9d43a3dccba0a73407ed24511a162abd4d170a
Ben Margevicius <bdm4@case.edu>
2016-09-13 17:18:29 -0400

    Adding EF Person Model for CF DB Deployment

---
+++ DirectoryPlus/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DirectoryPlus.Models
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CaseUserID { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ImageURL { get; set; }
+        public string HomePageURL { get; set; }
+        public string Prefix { get; set; }
+        public string Suffix { get; set; }
+        public string Title { get; set; }
+        public DateTime LastModified { get; set; }
+     }
+
+    public class Office
+    {
+
+    }
+}
