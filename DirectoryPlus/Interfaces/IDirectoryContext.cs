6eb1c1fede4b70572b6f4c3a6a34bcd08cf5c669
Ben Margevicius <bdm4@case.edu>
2016-09-14 13:49:23 -0400

    Changed protection levels on the db context for testing

---
+++ DirectoryPlus/Interfaces/IDirectoryContext.cs
@@ -0,0 +1,16 @@
+﻿using DirectoryPlus.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryPlus.Interfaces
+{
+    public interface IDirectoryContext
+    {
+        DbSet<Person> People { get; set; }
+        DbSet<Office> Offices { get; set; }
+    }
+}
