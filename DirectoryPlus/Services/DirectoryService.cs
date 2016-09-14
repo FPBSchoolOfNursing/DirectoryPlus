866d0be89612febda5c11ef675306f6136f03b02
Ben Margevicius <bdm4@case.edu>
2016-09-14 13:49:04 -0400

    Created a service stub

---
+++ DirectoryPlus/Services/DirectoryService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DirectoryPlus.Interfaces;
+using DirectoryPlus.Models;
+
+
+namespace DirectoryPlus.Services
+{
+    public class DirectoryService
+    {
+        private IDirectoryContext _context;
+
+        public DirectoryService(IDirectoryContext context)
+        {
+            _context = context;
+        }
+
+        public void AddDirectoryEntry(Person person)
+        {           
+            _context.People.Add(person);
+        }
+    }
+}
