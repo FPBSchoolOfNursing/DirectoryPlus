e4e47114d9e2f16d3f790a4c24f53e17b7215b99
Ben Margevicius <bdm4@case.edu>
2016-09-14 13:49:47 -0400

    Updated testing framework for in house db doubling

---
+++ DirectoryPlus/DataContexts/DirectoryContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using DirectoryPlus.Interfaces;
+using DirectoryPlus.Models;
+
+namespace DirectoryPlus.DataContexts
+{
+    public class DirectoryContext : DbContext, IDirectoryContext
+    {
+        public DirectoryContext() : base("name=DirectoryContext") { }
+
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {           
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
