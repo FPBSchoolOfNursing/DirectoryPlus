eed99e5a329ba0474f408bb38309e7504580e46d
Ben Margevicius <bdm4@case.edu>
2016-11-15 14:46:26 -0500

    Added CaseID Regex

---
+++ DirectoryPlus/Helpers/CaseUserIDRegex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace DirectoryPlus.Helpers
+{
+    public static class CaseUserIDRegex
+    {
+        public static bool isMatch(string CaseID)
+        {
+            Regex caseid = new Regex(@"/^[a-zA-Z]{3}([1-9]\d{0,3})?$/");
+            return (caseid.IsMatch(CaseID));           
+        }
+    }
+}
